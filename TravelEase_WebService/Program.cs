e6081ca0a4e431fa2cc865dc6681dd896bdf8165
chamathkavindya <it20122096@my.sliit.lk>
2023-10-23 09:31:49 +0530

    new changes

---
+++ TravelEase_WebService/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+------------------------------------------------------------------------------
+ File: Program.cs
+ Purpose: This file contains the program entry point and configuration setup
+ for the TravelEase_WebService project.
+ Author: IT20122096
+ Date: 2023-10-13
+------------------------------------------------------------------------------
+*/
+using TravelEase_WebService.Data;
+using TravelEase_WebService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TravelEase_WebService.Utils;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+// Configure database settings
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddControllers();
+
+// Configure authentication and authorization
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+// Configure CORS
+var myOrigins = "_myOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddRazorPages();
+
+// Register services and dependencies
+builder.Services.AddScoped<ITrainService, TrainService>();
+builder.Services.AddScoped<ITrainScheduleService, TrainScheduleService>();
+builder.Services.AddScoped<IUserService,UserService>();
+builder.Services.AddScoped<ITravelerService,TravelerService>();
+builder.Services.AddScoped<ITravelerAccountRequestService,TravelerAccountRequestService>(); 
+builder.Services.AddScoped<IReservationService,ReservationService>();
+builder.Services.AddScoped<PasswordEncryptionUtil>();
+
+
+//var host = builder.WebHost;
+//host.ConfigureKestrel(options =>
+//{
+//    options.Listen(System.Net.IPAddress.Parse("192.168.8.193"), 5065);
+//});
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.MapControllers();
+app.UseCors(myOrigins);
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpsRedirection();
+
+app.MapControllers();
+app.Run();
+
